9f889bfee5809e26145c587838453c14e767e3eb
Joe Sturzenegger <joesturz84@gmail.com>
2017-07-04 12:39:02 -0600

    Problem 5 complete
    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?

---
+++ ProjectEuler/ProjectEuler/problem5.swift
@@ -0,0 +1,63 @@
+//
+//  problem5.swift
+//  ProjectEuler
+//
+//  Created by Joe Sturzenegger on 7/4/17.
+//  Copyright © 2017 Joe Sturzenegger. All rights reserved.
+//
+
+import Foundation
+class Problem5
+{
+  static func getSmallestMultiple(of number: Int) -> Int
+  {
+    let max = (1...number).reduce(1, *)
+    let divisors = getListOf(divisorsFor: number)
+    for i in stride(from: number, to: max, by: number) {
+      if(can(integer: i, beDivisibleBy: divisors))
+      {
+        return i
+      }
+    }
+    return 0
+  }
+  
+  static func can(integer number: Int, beDivisibleBy divisors: Set<Int>) -> Bool
+  {
+    for i in divisors
+    {
+      if number % i != 0
+      {
+        return false
+      }
+    }
+    return true
+  }
+  
+  static func getListOf(divisorsFor number: Int) -> Set<Int>
+  {
+    var values = Set<Int>()
+    for i in (2...number).reversed()
+    {
+      values.insert(i)
+    }
+    return removeExtraDivisorsFrom(Set: values)
+  }
+  
+  static func removeExtraDivisorsFrom(Set: Set<Int>) -> Set<Int>
+  {
+    var result: Set = Set
+    for i in Set {
+      let removeList = Problem3.listDivisors(number: i)
+      for j in removeList
+      {
+        if(j < i)
+        {
+          result.remove(j)
+        }
+      }
+    }
+    return result
+  }
+  
+}
