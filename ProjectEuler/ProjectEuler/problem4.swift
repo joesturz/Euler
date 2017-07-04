230443710ae932654b5cd7fb674483a6e948bda2
Joe Sturzenegger <joesturz84@gmail.com>
2017-07-03 22:30:55 -0600

    Problem 4 complete
    Find the largest palindrome made from the product of two 3-digit numbers.

---
+++ ProjectEuler/ProjectEuler/problem4.swift
@@ -0,0 +1,32 @@
+//
+//  problem4.swift
+//  ProjectEuler
+//
+//  Created by Joe Sturzenegger on 7/3/17.
+//  Copyright © 2017 Joe Sturzenegger. All rights reserved.
+//
+
+import Foundation
+
+class Problem4 {
+  static func getLargestPalindrome(max: Int) -> Int{
+    let end = (max / 9) - 1
+    var result = [Int]()
+    for i in (end...max).reversed() {
+      let temp = i
+      for j in (end...temp).reversed() {
+        let value = i * j
+        if(isPalindrome(number: value)){
+          result.append(value)
+        }
+      }
+    }
+    return result.max()!
+  }
+  
+  private static func isPalindrome(number: Int) -> Bool {
+    let stringified =  String(number)
+    let reversedString = String(stringified.characters.reversed())
+    return stringified == reversedString
+  }
+}
