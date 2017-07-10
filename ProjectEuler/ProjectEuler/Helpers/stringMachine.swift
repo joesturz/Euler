959b3794be412728a675395bfd70664590d49b28
Joe Sturzenegger <joesturz84@gmail.com>
2017-07-09 22:22:45 -0600

    Library Addition
    Added a library for string manipulation and created a folder for helper libraries

---
+++ ProjectEuler/ProjectEuler/Helpers/stringMachine.swift
@@ -0,0 +1,20 @@
+//
+//  stringMachine.swift
+//  ProjectEuler
+//
+//  Created by Joe Sturzenegger on 7/9/17.
+//  Copyright © 2017 Joe Sturzenegger. All rights reserved.
+//
+
+import Foundation
+
+class StringMachine
+{
+  static func getSubstringFromString(_ string: String, from start: Int, to end: Int) -> String?
+  {
+    let startIndex = string.index(string.startIndex, offsetBy: start)
+    let endIndex = string.index(string.startIndex, offsetBy: end)
+    let sub = String(string.substring(with: (startIndex..<endIndex)))
+    return sub
+  }
+}
