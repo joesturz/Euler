f3e2fc909a6a7de8dfe2b7d596965f043a2dddab
Joe Sturzenegger <joesturz84@gmail.com>
2017-08-08 18:01:14 -0600

    Problem 67 setup

---
+++ ProjectEuler/ProjectEuler/Sources/Problem67.swift
@@ -0,0 +1,15 @@
+//
+//  Problem67.swift
+//  ProjectEuler
+//
+//  Created by Joe Sturzenegger on 8/8/17.
+//  Copyright © 2017 Joe Sturzenegger. All rights reserved.
+//
+
+import Foundation
+class Problem67
+{
+  //see Problem18 to compute the data
+  static let reallyBigTriangle = []
+
+}
