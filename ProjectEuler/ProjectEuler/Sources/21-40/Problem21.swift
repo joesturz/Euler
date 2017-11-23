866c2b40eac2baf4052e229dccfb6579d5bfa070
Joe Sturzenegger <joesturz84@gmail.com>
2017-11-22 21:37:52 -0700

    Problem 21 complete
    Evaluate the sum of all the amicable numbers under 10000.

---
+++ ProjectEuler/ProjectEuler/Sources/21-40/Problem21.swift
@@ -0,0 +1,52 @@
+//
+//  Problem21.swift
+//  ProjectEuler
+//
+//  Created by Joe Sturzenegger on 11/22/17.
+//  Copyright © 2017 Joe Sturzenegger. All rights reserved.
+//
+
+import Foundation
+class Problem21
+{
+  static var max:Int = 0
+  static func getSumOfAmicableNumbersUnder(num: Int) -> Int {
+    max = num
+    var setOfNumbers:Set<Int> = []
+    if num < 4
+    {
+      return 0
+    }
+    for i in (4...num)
+    {
+      if !Prime.determinePrime(number: i)
+      {
+        let (num1, num2) = getAmicablePair(num: i)
+        setOfNumbers.insert(num1)
+        setOfNumbers.insert(num2)
+      }
+    }
+    return setOfNumbers.reduce(0, +)
+  }
+  
+  static func getAmicablePair(num: Int) -> (Int, Int){
+    var set1 = Prime.setOfDivisors(number: num)
+    set1.remove(num)
+    let sum1 = set1.reduce(0, +)
+    if sum1 > max || sum1 == num
+    {
+      return (0,0)
+    }
+    var set2 = Prime.setOfDivisors(number: sum1)
+    set2.remove(sum1)
+    let sum2 = set2.reduce(0, +)
+    if sum2 == num
+    {
+      return (num, sum1)
+    }
+    else
+    {
+      return (0,0)
+    }
+  }
+}
